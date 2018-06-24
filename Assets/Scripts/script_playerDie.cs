dab5943c08d5a183e487306e5784ec6166349aca
TongueEnvy <mr.dfelkins@gmail.com>
2018-06-23 19:52:08 -0700

    Blood for the blood god!
    -players explode into a shower of blood on death

--- Assets/Scripts/script_playerDie.cs
+++ Assets/Scripts/script_playerDie.cs
@@ -5,9 +5,14 @@ using UnityEngine;
 public class script_playerDie : MonoBehaviour {
 
     public GameObject scoreKeeper;
+    public GameObject bloodDrop;
+    public GameObject bloodStainFloor;
+    public GameObject bloodStainBall;
+    public float dropletHorSpeed;
+    public float dropletVertSpeed;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 
         scoreKeeper = GameObject.Find("ScoreKeeper");
 
@@ -19,6 +24,40 @@ public class script_playerDie : MonoBehaviour {
 
         if (collision.gameObject.tag == "Ball")
         {
+
+            var newSplatter = Instantiate<GameObject>(bloodStainFloor);
+            newSplatter.transform.parent = gameObject.transform;
+            newSplatter.transform.localPosition = Vector3.zero;
+            newSplatter.transform.eulerAngles = new Vector3(0, Random.Range(0, 360), 0);
+            newSplatter.transform.parent = null;
+
+            newSplatter = Instantiate<GameObject>(bloodStainBall);
+            newSplatter.transform.parent = collision.gameObject.transform;
+            newSplatter.transform.localPosition = Vector3.zero;
+            newSplatter.transform.eulerAngles = new Vector3(0, Random.Range(0, 360), 0);
+
+            var dropletCount = (int)Random.Range(5, 10);
+            for(var i = 0; i <= dropletCount; i += 1)
+            {
+
+                var newDroplet = Instantiate<GameObject>(bloodDrop);
+                newDroplet.transform.parent = gameObject.transform;
+                newDroplet.transform.localPosition = new Vector3(0, .5f, 0);
+                var dropScaleFactor = Random.Range(.1f, .25f);
+                newDroplet.transform.parent = null;
+                newDroplet.transform.localScale = new Vector3(dropScaleFactor, dropScaleFactor, dropScaleFactor);
+                newDroplet.GetComponent<Rigidbody>().velocity = new Vector3(Random.Range(-dropletHorSpeed, dropletHorSpeed), Random.Range(0, dropletVertSpeed), Random.Range(-dropletHorSpeed, dropletHorSpeed));
+                var vectorCheck = new Vector3(newDroplet.GetComponent<Rigidbody>().velocity.x, 0, newDroplet.GetComponent<Rigidbody>().velocity.z);
+                if(vectorCheck.magnitude > dropletHorSpeed)
+                {
+
+                    vectorCheck.Normalize();
+                    vectorCheck = vectorCheck * Random.Range(-dropletHorSpeed, dropletHorSpeed);
+                    newDroplet.GetComponent<Rigidbody>().velocity = new Vector3(vectorCheck.x, newDroplet.GetComponent<Rigidbody>().velocity.y, vectorCheck.z);
+
+                }
+            }
+
             if (scoreKeeper.GetComponent<script_trackScore>().roundIsDecided == false)
                 {
 
