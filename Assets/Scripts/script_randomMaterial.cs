4680d1a0ee21ec4c6f413f2defa0bd9f1649732b
TongueEnvy <mr.dfelkins@gmail.com>
2018-06-29 16:40:56 -0700

    Added bats and input axies

---
+++ Assets/Scripts/script_randomMaterial.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class script_randomMaterial : MonoBehaviour {
+
+    public List<Material> materials;
+
+	// Use this for initialization
+	void Start () {
+
+        var newMat = (int)Random.Range(0, materials.Count);
+        gameObject.GetComponent<MeshRenderer>().material = materials[newMat];
+
+
+    }
+
+    public void ChangeMaterial()
+    {
+
+        var newMat = (int)Random.Range(0, materials.Count);
+        gameObject.GetComponent<MeshRenderer>().material = materials[newMat];
+
+    }
+}
